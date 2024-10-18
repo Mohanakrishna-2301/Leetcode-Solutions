c38ae0f55c1382ecebae34040149270b581937bf
Mohanakrishna-2301 <mohanakrishna2301@gmail.com>
2024-10-18 20:43:51 +0530

    Problem 2044

--- Leetcode_2044.c
+++ Leetcode_2044.c
@@ -4,10 +4,12 @@ int max_or = 0;  // To store the maximum bitwise OR
 int result = 0;  // To count the number of subsets with max bitwise OR
 
 // DFS function to explore all subsets
-void dfs(int *nums, int size, int index, int current_or) {
+void dfs(int *nums, int size, int index, int current_or) 
+{
     // If we've checked all elements, compare the current OR with the max OR
     if (index == size) {
-        if (current_or == max_or) {
+        if (current_or == max_or)
+        {
             result++;  // Increment the count if the current OR equals the max
         }
         return;
@@ -20,9 +22,11 @@ void dfs(int *nums, int size, int index, int current_or) {
     dfs(nums, size, index + 1, current_or | nums[index]);
 }
 
-int countMaxOrSubsets(int* nums, int numsSize) {
+int countMaxOrSubsets(int* nums, int numsSize) 
+{
     // Find the maximum possible OR value by OR-ing all elements
-    for (int i = 0; i < numsSize; i++) {
+    for (int i = 0; i < numsSize; i++) 
+    {
         max_or |= nums[i];
     }
     
@@ -32,7 +36,8 @@ int countMaxOrSubsets(int* nums, int numsSize) {
     return result;  // Return the number of subsets achieving the max OR
 }
 
-int main() {
+int main() 
+{
     int nums[] = {3, 2, 1, 5};  // Example input
     int size = sizeof(nums) / sizeof(nums[0]);
     printf("Number of maximum OR subsets: %d\n", countMaxOrSubsets(nums, size));
